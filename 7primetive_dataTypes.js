dde68d6af940676eee2f916cbc589dd3d0e0546e
Suman Paik <sumanpaikcs@gmail.com>
2022-07-30 21:43:55 +0530

    some file added

---
+++ 7primetive_dataTypes.js
@@ -0,0 +1,11 @@
+let a = 34;
+let b = null;
+let c = "suman";
+let d = true;
+let e = BigInt("23")+BigInt("7");
+let f = undefined;
+let g = Symbol("this is a nice language");
+console.log(a, b, c, d, e, f, g)
+console.log(typeof e)
+console.log(typeof c)
+console.log(typeof d)
---
+++ first.js
@@ -0,0 +1,22 @@
+console.log("this is javascript tutorial")
+// var a = 23
+// var b = "Suman"
+// var c = null
+// var d = undefined
+// {
+//     var b = "paik"
+//     console.log(b)
+// }
+// console.log(b)
+
+let a = 23
+let b = "Suman";
+const author = "suman paik"
+let c = null
+let d = undefined
+{
+    let b = "paik"
+    console.log(b)
+}
+console.log(b)
+
